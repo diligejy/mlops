6510bbb311a242728c031e5da4f729df5d08da99
diligejy <sjy049@gmail.com>
2024-01-18 11:23:04 +0900

    test

--- Practical_MLOps/Ch1/hello.py
+++ Practical_MLOps/Ch1/hello.py
@@ -1,7 +1,15 @@
+import numpy as np 
+
 def add(x, y):
     """This is an add function"""
     return x + y
 
+def numpy_add(x, y):    
+    return x + y 
+
+
 print(add(1, 1))
 
 print(add(2, 1))
+
+print(numpy_add(np.arange(4), np.arange(4)))
