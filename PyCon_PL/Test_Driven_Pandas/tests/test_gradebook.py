c4731aa0adea473ae7b713249e06fd4d3a13ed94
diligejy <sjy049@gmail.com>
2024-01-19 21:13:16 +0900

    TDD Pandas - [RED] Prepare the first input data

--- PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
+++ PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
@@ -1,2 +1,12 @@
+import pandas as pd 
+
 def test_x():
+    students = [{
+        "ID" : 1,
+        "Name" : "Doe, John",
+        "NetID" : "JXD12345",
+        "Email Address" : "JOHN.DOE@EXAMPLE.EDU",
+        "Group" : 1,
+    }]
+    students_df = pd.DataFrame(data=students).set_index("ID")
     assert False
