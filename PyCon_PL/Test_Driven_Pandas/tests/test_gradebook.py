eeb6a253ee0e8cbc2b25f228e149d279452fc989
diligejy <sjy049@gmail.com>
2024-01-20 09:58:16 +0900

    TDD Pandas - [RED/GREEN] Calculate homework average for a single homework

--- PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
+++ PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
@@ -1,22 +1,34 @@
 import pandas as pd 
 import pytest 
-from typing import cast
+from typing import cast, Optional
 
-def _create_group(raw_students_group:pd.DataFrame) -> pd.DataFrame:
-    result = pd.DataFrame(index=raw_students_group.index)
+def _create_group(students_with_scores:pd.DataFrame) -> pd.DataFrame:
+    result = pd.DataFrame(index=students_with_scores.index)
     result = result.assign(
-        net_id = raw_students_group.index,
-        email_address = raw_students_group['Email Address'].str.lower(),
+        net_id = students_with_scores.index,
+        email_address = students_with_scores['Email Address'].str.lower(),
     )
-    result[['last_name', 'first_name']] = raw_students_group['Name'].str.split(", ", expand=True)
+    result[['last_name', 'first_name']] = students_with_scores['Name'].str.split(", ", expand=True)
+    result['homework_average'] = (students_with_scores["homework_1"] / students_with_scores["homework_1_max_points"])
     return result 
 
-def generate_gradebook(students_df:pd.DataFrame) -> dict[int, pd.DataFrame]:
-    print(
-        'This is index', students_df.index)
+
+def generate_gradebook(students_df:pd.DataFrame, homework_exams_df: Optional[pd.DataFrame] = None) -> dict[int, pd.DataFrame]:
     students_df.index = students_df.index.str.lower()
+    if homework_exams_df is None:
+        homework_exams_df = pd.DataFrame(index=students_df.index)
+        homework_exams_df = homework_exams_df.assign(
+            homework_1=0,
+            homework_1_max_points=1,
+        )
+    students_with_scores = pd.merge(
+        students_df,
+        homework_exams_df,
+        left_index=True,
+        right_index=True
+    )
     return {
-        cast(int, group): _create_group(raw_students_group=table)
+        cast(int, group): _create_group(students_with_scores=students_with_scores)
         for group, table in students_df.groupby("Group")
     }
     
@@ -106,3 +118,30 @@ def test_results_group_contains_students_last_name(
         "Doe",
         "Doe",
     ]
+    
+def test_results_group_contains_students_homework_average_for_single_homework():
+    students = [{
+        "ID" : 1,
+        "Name" : "Doe, John",
+        "NetID" : "JXD12345",
+        "Email Address" : "JOHN.DOE@EXAMPLE.EDU",
+        "Group" : 1,
+    }]
+    students_df = pd.DataFrame(data=students).set_index("NetID")
+    
+    homework_exams = [
+        {
+            "First Name": "John",
+            "Last Name": "Doe",
+            "SID": "jxd12345",
+            "homework_1": 25,
+            "homework_1_max_points": 50,
+        }
+    ]
+    homework_exams_df = pd.DataFrame(data=homework_exams).set_index("SID")
+    result = generate_gradebook(
+        students_df=students_df,
+        homework_exams_df=homework_exams_df,
+    )
+    
+    assert result[1]["homework_average"].to_list() == [0.5]
