fd4d58cbf2e0940a84254384e4aa291dd818e0f2
diligejy <sjy049@gmail.com>
2024-01-20 09:00:15 +0900

    TDD Pandas - [RED/GREEN] Adding email_address column

--- PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
+++ PyCon_PL/Test_Driven_Pandas/tests/test_gradebook.py
@@ -5,6 +5,7 @@ def _create_group(raw_students_group:pd.DataFrame) -> pd.DataFrame:
     result = pd.DataFrame()
     return result.assign(
         net_id = raw_students_group["NetID"].str.lower(),
+        email_address = raw_students_group['Email Address'].str.lower(),
     )
 
 def generate_gradebook(students_df:pd.DataFrame) -> dict[int, pd.DataFrame]:
@@ -51,14 +52,14 @@ def test_results_group_contains_students_net_id_lowercase():
             "ID": 1,
             "Name": "Doe, John",
             "NetID": "JXD12345",
-            "Email Adress": "JOHN.DOE@EXAMPLE.EDU",
+            "Email Address": "JOHN.DOE@EXAMPLE.EDU",
             "Group": 1,
         },
         {
             "ID": 2,
             "Name": "Doe, Second",
             "NetID": "SXD54321",
-            "Email Adress": "SECOND.DOE@EXAMPLE.EDU",
+            "Email Address": "SECOND.DOE@EXAMPLE.EDU",
             "Group": 1,
         },
     ]
@@ -67,3 +68,29 @@ def test_results_group_contains_students_net_id_lowercase():
     result = generate_gradebook(students_df=students_df)
 
     assert result[1]["net_id"].to_list() == ["jxd12345", "sxd54321"]
+    
+def test_results_group_contains_students_email_address_lowercase():
+    students = [
+        {
+            "ID": 1,
+            "Name": "Doe, John",
+            "NetID": "JXD12345",
+            "Email Address": "JOHN.DOE@EXAMPLE.EDU",
+            "Group": 1,
+        },
+        {
+            "ID": 2,
+            "Name": "Doe, Second",
+            "NetID": "SXD54321",
+            "Email Address": "SECOND.DOE@EXAMPLE.EDU",
+            "Group": 1,
+        },
+    ]
+    students_df = pd.DataFrame(data=students).set_index("ID")
+
+    result = generate_gradebook(students_df = students_df)
+    
+    assert result[1]['email_address'].to_list() == [ 
+                                                    'john.doe@example.edu',
+                                                    'second.doe@example.edu'
+                                                    ]
